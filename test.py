17f658564c3790d4c5c8a05b9e2aecd9e001ae31
leon <leonl2@bitbucket.org>
2019-12-09 00:19:47 -0500

    first commit

---
+++ test.py
@@ -0,0 +1,25 @@
+import json
+from datetime import datetime
+
+header = '<!doctype html><html><head><title>CS1410 &middot; TRON Results</title><meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"><meta http-equiv="x-ua-compatible" content="ie=edge"><link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"><link rel="stylesheet" href="./results.css"></head>'
+bodyStart = '<body><header class="default-header"><h1>TRON-41</h1></header><main class="container"><div class="table-responsive"><table class="table"><thead><tr><th></th><th>Player</th><th>Score</th><th>ELO</th></tr></thead><tbody>'
+tronResults = ""
+index = 0
+with open("test1.txt") as f:
+	data = json.load(f)
+	for d in data:
+		print("===========CUT 1===============")
+		for d2 in d:
+			print("xxxxxxxxxxxxxxCUT 2xxxxxxxxxxxxx")
+			for d3 in d2:
+				tronResults += "<pre>" + d3 + "</pre>"
+        
+
+bodyEnd = '</tbody></table></div><p id="date">Published on {0}</p></main></body></html>'.format(
+    datetime.today().strftime("%b %d at %I:%M %p")
+)
+
+html_results = header + bodyStart + tronResults + bodyEnd
+out_path = "tron_results.html"
+with open(out_path, "w") as out:
+    out.write(html_results)
